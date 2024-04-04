cb1a957ec8f2c553b7b16cf06f1358edcc6dec65
Daniel Bretoi <daniel@otherware.org>
2024-04-03 20:16:46 -0700

    getting syncDir

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This is your new Joplin plugin. It is suggested that you use this README file to document your plugin.
 
 For information on how to build or publish the plugin, please see [GENERATOR_DOC.md](./GENERATOR_DOC.md)
+
+# Libraries used
+- https://github.com/isomorphic-git/isomorphic-git
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import joplin from 'api';
+import { promises as fs } from 'fs';
+
 
 joplin.plugins.register({
 	onStart: async function() {
 		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
+		const profileDir = await joplin.settings.globalValue('profileDir');
+		const settingsData = await fs.readFile(`${profileDir}/settings.json`, 'utf8');
+		const settings = JSON.parse(settingsData);
+		console.info(settings['sync.2.path']);
+		console.info('Plugin started!');
 	},
 });
